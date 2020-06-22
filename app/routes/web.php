f56def9317641d68d92b177f942015c021905740
youssefcharfi <youssefcharfipro@gmail.com>
2020-06-23 00:47:43 +0100

    test1

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,7 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('/', function () {
+    return view('test');
+});
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
