baf4b5555334c5ae73cfbd32ac43349ac6fed938
Mohamed <65641001+MohamedBenTaher@users.noreply.github.com>
2020-07-06 01:50:53 +0100

    routes

--- routes/web.php
+++ routes/web.php
@@ -14,19 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-<<<<<<< HEAD
-Route::get('/', function () {
-    return view('welcome');
-});
-
-=======
->>>>>>> dcd04081b605ab196666bc8723e598cc9d630cc5
 /*Route::get('/', function () {
     return view('test');
 });*/
 Auth::routes();
 
-<<<<<<< HEAD
 
 
 Route::get('/home', 'HomeController@index')->name('home');
@@ -37,7 +29,5 @@ Route::get('comments/comment','commentController@index');
 Route::post('commentdelete','commentController@delete');
 Route::post('commentupdate','commentController@update');
 Route::post('commentsubmit','commentController@submit');
-=======
 Route::resource('/vols', 'VolController');
 Route::resource('/entreprise','EntrepriseController');
->>>>>>> dcd04081b605ab196666bc8723e598cc9d630cc5
