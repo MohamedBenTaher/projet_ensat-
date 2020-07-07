2a8baffafc0e9c6a9dae7286c124c12ab9aab062
Mohamed <65641001+MohamedBenTaher@users.noreply.github.com>
2020-07-07 16:00:03 +0100

    edit vol

--- resources/views/vols/edit.blade.php
+++ resources/views/vols/edit.blade.php
@@ -1,52 +1,51 @@
-@extends('layouts.travel')
- 
+@extends('travel.blade')
 @section('content')
-<h2 style="margin-top: 12px;" class="text-center">Ajouter Un Vol</a></h2>
+<h2 style="margin-top: 12px;" class="text-center">mettre à jour un vol</a></h2>
 <br>
  
-<form action="{{ route('vols.store')}}" method="POST" name="add_vol">
+<form action="{{route('vols.update',['id'=>$vol->id])}}" method="POST" name="add_vol">
 @csrf
- 
+ @method('PUT')
 <div class="row">
     <div class="col-md-12">
         <div class="form-group">
             <strong>Ville de Départ :</strong>
-            <input type="text" name="ville_dep" class="form-control" placeholder="ville de départ">
+            <input type="text" name="ville_dep" class="form-control" placeholder={{old($vol['ville_dep'])}}>
             <span class="text-danger">{{ $errors->first('Nom') }}</span>
         </div>
     </div>
     <div class="col-md-12">
         <div class="form-group">
             <strong>Ville D'arrivé </strong>
-            <input type="text" name="ville_arr" class="form-control" placeholder="ville d'arrivé">
+            <input type="text" name="ville_arr" class="form-control" placeholder={{old($vol['ville_arr'])}}>
             <span class="text-danger">{{ $errors->first('title') }}</span>
         </div>
     </div>
     <div class="col-md-12">
         <div class="form-group">
             <strong>Date De Départ:</strong>
-            <input type="date" name="date_dep" class="form-control" placeholder="Entrer une date de départ">
+            <input type="date" name="date_dep" class="form-control" placeholder={{old($vol['date_dep'])}}>
             <span class="text-danger">{{ $errors->first('date_dep') }}</span>
         </div>
     </div>
     <div class="col-md-12">
         <div class="form-group">
             <strong>Date D'arrivé':</strong>
-            <input type="date" name="date_arr" class="form-control" placeholder="Entrer une date d'arrivé">
+            <input type="date" name="date_arr" class="form-control" placeholder={{old($vol['date_arr'])}}>
             <span class="text-danger">{{ $errors->first('date_arr') }}</span>
         </div>
     </div>
     <div class="col-md-12">
         <div class="form-group">
             <strong>Heure de départ:</strong>
-            <input type="time" name="heure_dep" class="form-control" placeholder="Entrer une heure de départ">
+            <input type="time" name="heure_dep" class="form-control" placeholder={{old($vol['heure_dep'])}}>
             <span class="text-danger">{{ $errors->first('heure_dep') }}</span>
         </div>
     </div>
     <div class="col-md-12">
             <div class="form-group">
             <strong>Heure d'arrivé:</strong>
-            <input type="time" name="heure_arr" class="form-control" placeholder="Entrer une heure d'arrivé">
+            <input type="time" name="heure_arr" class="form-control" placeholder={{old($vol['heure_arr'])}}>
             <span class="text-danger">{{ $errors->first('heure_arr') }}</span>
         </div>
     </div>
@@ -54,7 +53,7 @@
     <div class="col-md-12">
         <div class="form-group">
             <strong>Classe</strong>
-            <input type="text" name="classe" class="form-control" placeholder="Entrer la classe du vol ">
+            <input type="text" name="classe" class="form-control" placeholder={{old($vol['classe'])}}>
             <span class="text-danger">{{ $errors->first('classe') }}</span>
         </div>
     </div>
@@ -71,13 +70,13 @@
     <div class="col-md-12">
         <div class="form-group">
             <strong>Image :</strong>
-            <input type="file" name="image" class="form-control"  >
+            <input type="file" name="image" class="img"  >
             <span class="text-danger">{{ $errors->first('image') }}</span>
         </div>
     </div>
     
     <div class="col-md-12">
-       <input type="submit" value="Ajouter" class="btn btn-primary">
+       <input type="submit" value="mettre à jour" class="btn btn-primary">
     </div>
 </div>
  
