15e716b5d57b0aaebe353751c5df6ac2daae6193
Mehdi Mansour <mansourmehdi62@gmail.com>
2020-07-09 22:56:18 +0100

    commentresource

--- routes/web.php
+++ routes/web.php
@@ -22,11 +22,15 @@ Auth::routes();
 
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('avis/avis', 'avisController@index');
 Route::resource('vols','VolController');
-Route::post('comments/comment','commentController@store');
+/*Route::post('comments/comment','commentController@store');
 Route::get('comments/comment','commentController@index');
 Route::post('commentdelete','commentController@delete');
 Route::post('commentupdate','commentController@update');
-Route::post('commentsubmit','commentController@submit');
+Route::post('commentsubmit','commentController@submit');*/
 Route::resource('/entreprise','EntrepriseController');
+Route::resource('/comments','commentsController');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
