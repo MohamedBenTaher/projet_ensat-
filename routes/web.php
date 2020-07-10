0c299ccf864a76899745f2a4d0aa4ee5eeba155c
Mohamed <65641001+MohamedBenTaher@users.noreply.github.com>
2020-07-10 12:00:46 +0100

    controller

--- routes/web.php
+++ routes/web.php
@@ -24,21 +24,6 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 /*  Route::get('avis/avis', 'avisController@index');*/
 
-<<<<<<< HEAD
-Route::get('/home', 'HomeController@index')->name('home');
-Route::resource('vols','VolController');
-/*Route::post('comments/comment','commentController@store');
-Route::get('comments/comment','commentController@index');
-Route::post('commentdelete','commentController@delete');
-Route::post('commentupdate','commentController@update');
-Route::post('commentsubmit','commentController@submit');*/
-Route::resource('/entreprise','EntrepriseController');
-Route::resource('/comments','commentsController');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-=======
 Route::resource('vols','VolController');
 
 /*
@@ -50,4 +35,3 @@ Route::post('commentsubmit','commentController@submit');
 */
 
 Route::resource('/entreprise','EntrepriseController');
->>>>>>> c9be6f8f8bd883fdfd20de6d2cc3698c65cff826
