0c3795e8e84c92280d873dd8e078745a45a0f8b9
Mohamed <65641001+MohamedBenTaher@users.noreply.github.com>
2020-07-12 00:24:36 +0100

    route

--- routes/web.php
+++ routes/web.php
@@ -23,27 +23,8 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 
-<<<<<<< HEAD
-Route::get('/home', 'HomeController@index')->name('home');
-Route::resource('vols','VolController');
-Route::post('comments/comment','commentsController@store');
-Route::get('comments/comment','commentsController@index');
-Route::post('commentdelete','commentsController@destroy');
-Route::post('commentupdate','commentsController@update');
-Route::post('commentsubmit','commentsController@show');
-Route::resource('/entreprise','EntrepriseController');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-=======
->>>>>>> 8b817a26e63d7761591ce41cd05238c0287f10fe
 Route::resource('vols','VolController');
 Route::post('likes','likesController@store');
 Route::post('dislikes','dislikesController@store');
 
-<<<<<<< HEAD
-Route::resource('/entreprise','EntrepriseController');
-=======
 Route::resource('entreprise','EntrepriseController');
->>>>>>> 8b817a26e63d7761591ce41cd05238c0287f10fe
